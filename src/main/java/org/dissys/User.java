5529a1773953808513bcccfe48fb6a63a5ee570a
Valerio Xie <valerioyong.xie@mail.polimi.it>
2024-08-05 17:17:54 +0200

    User Data structure

---
+++ src/main/java/org/dissys/User.java
@@ -0,0 +1,26 @@
+package org.dissys;
+
+import java.util.UUID;
+
+class User {
+    private final UUID id;
+    private String name;
+
+    public User(String name) {
+        this.id = UUID.randomUUID();
+        this.name = name;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}

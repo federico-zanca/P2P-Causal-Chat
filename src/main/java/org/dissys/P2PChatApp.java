b94ca15f8c1ae36e20142cb2e16a3bc7ef3e6614
Valerio Xie <valerioyong.xie@mail.polimi.it>
2024-08-10 12:03:48 +0200

    pulito un paio di classi

--- src/main/java/org/dissys/P2PChatApp.java
+++ src/main/java/org/dissys/P2PChatApp.java
@@ -1,14 +1,9 @@
 package org.dissys;
 
 import org.dissys.network.Client;
-import org.dissys.network.NATTraversal;
 
 import java.io.IOException;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
